7cc87228cd0f3674731ab5b576ec234813bcd943
W. Ross Morrow <wrossmorrow@stanford.edu>
2018-04-27 10:34:07 -0700

    edited readme and qualtrics.js

--- qualtrics.js
+++ qualtrics.js
@@ -1,14 +1,18 @@
 Qualtrics.SurveyEngine.addOnload( function()
 {
+	// get place to store custom HTML for reviews...
 	var container = this.getQuestionTextContainer();
-	fetch( "https://my.server.com/reviewsampler/api/review" )
+	// call review sampler API's main routine
+	fetch( "https://my.server.com/reviewsampler/api/get/review" )
 		.then( data => {
 			// process data recieved as JSON data
 			data.json()
-				.then( json => ( 
+				.then( json => {
 					// Replace question's text container with a custom format of the response 
-					container.innerHTML = "<ins>Question 1</ins><br><ul><li/><b>Product:</b> " + Product + " <br><li/><b>Rating:</b> " + Rating + "/5 stars <br><li/><b>Review:</b> " + Review + "</ul>";
-				) )
+					container.innerHTML = "<ins>Question 1</ins><br><ul><li/><b>Product:</b> " + json.Product + " <br><li/><b>Rating:</b> " + json.Rating + "/5 stars <br><li/><b>Review:</b> " + json.Review + "</ul>";
+					// store the Review ID embedded data so we have it later for response analysis
+					Qualtrics.SurveyEngine.setEmbeddedData('R1',json.ReviewId);
+				} )
 			} , error => {
 				// Dump error, if we can, to the server. Otherwise we have no window into client-side errors. 
 				var postOpt = { method : 'post', body : JSON.stringify( error ) };
