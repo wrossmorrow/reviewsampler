29311f4a0e4f7a6277905da7efc86a1812e3240d
W. Ross Morrow <wrossmorrow@stanford.edu>
2018-04-28 08:02:15 -0700

    added questin index in qualtrics.js

--- qualtrics.js
+++ qualtrics.js
@@ -1,17 +1,18 @@
 Qualtrics.SurveyEngine.addOnload( function()
 {
+	// question index. change this in different questions, so we don't have to make multiple changes below. 
+	var questionIndex = 1;
 	// get place to store custom HTML for reviews...
 	var container = this.getQuestionTextContainer();
 	// call review sampler API's main routine
 	fetch( "https://my.server.com/reviewsampler/api/get/review" )
 		.then( data => {
 			// process data recieved as JSON data
-			data.json()
-				.then( json => {
+			data.json().then( json => {
 					// Replace question's text container with a custom format of the response 
-					container.innerHTML = "<ins>Question 1</ins><br><ul><li/><b>Product:</b> " + json.Product + " <br><li/><b>Rating:</b> " + json.Rating + "/5 stars <br><li/><b>Review:</b> " + json.Review + "</ul>";
+					container.innerHTML = "<ins>Question " + questionIndex + "</ins><br><ul><li/><b>Product:</b> " + json.Product + " <br><li/><b>Rating:</b> " + json.Rating + "/5 stars <br><li/><b>Review:</b> " + json.Review + "</ul>";
 					// store the Review ID embedded data so we have it later for response analysis
-					Qualtrics.SurveyEngine.setEmbeddedData('R1',json.ReviewId);
+					Qualtrics.SurveyEngine.setEmbeddedData( 'R' + questionIndex , json.ReviewId );
 				} )
 			} , error => {
 				// Dump error, if we can, to the server. Otherwise we have no window into client-side errors. 
