03917c4b87eb07f258346754dd465dfc547fabc7
W. Ross Morrow <wrossmorrow@stanford.edu>
2018-05-21 06:52:25 -0700

    added test script

--- test/test.sh
+++ test/test.sh
@@ -8,9 +8,9 @@ curl localhost:4050/sheet/load \
 		-XPOST -H "Content-type: application/json" \
 		-d '{ "spreadsheetId" : "1mkZV-HqNhW3jIprvaO6rMSlMswyqFdGng7qbtqhQj1Y" , "range" : "Sheet1!A2:D6" }'
 
-> testout.log
+> testout.txt
 for i in $( seq 1 100 ) ; do 
-	curl -s localhost:4050/get/review >> testout.log
-	echo '' >> testout.log
+	curl -s localhost:4050/get/review >> testout.txt
+	echo '' >> testout.txt
 	sleep 0.1
 done
